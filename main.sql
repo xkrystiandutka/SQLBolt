2959ebe51a72251d41a8111e5185310062406aec
Krystian <Krystian.Dutkait@gmail.com>
2022-05-07 20:47:17 +0200

    Add exercise 3

--- main.sql
+++ main.sql
@@ -37,3 +37,21 @@ SELECT * FROM movies where YEAR NOT BETWEEN 2000 AND 2010
 -- Find the first 5 Pixar movies and their release year
 
 SELECT * FROM Movies where id BETWEEN 1 AND 5;
+
+-- Exercise 3 — Tasks
+
+-- Find all the Toy Story movies 
+
+SELECT * FROM movies where title like "Toy Story%";
+
+-- Find all the movies directed by John Lasseter
+
+SELECT * FROM movies where director like "John Lasseter";
+
+-- Find all the movies (and director) not directed by John Lasseter
+
+SELECT * FROM movies where director not like "John Lasseter";
+
+-- Find all the WALL-* movies
+
+SELECT * FROM movies where title like "WALL-%";
